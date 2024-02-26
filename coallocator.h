cc729e993237e03b3fa928ea6be2a93e42bee290
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-02-26 14:04:50 +1100

    change err code in coallocator to be more consistent

--- coallocator.h
+++ coallocator.h
@@ -26,7 +26,7 @@ void *alloc(allocator_t *allocator, uint64_t size) {
 
 int allocator_init(void *backing_memory, uint64_t size, allocator_t *allocator) {
     if (!backing_memory || !size) {
-        return -1;
+        return 1;
     }
 
     for (uint64_t i = 0; i < size; i++) {
