e114da81aee3d7be45c56ae8c2f861d34664bc28
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-11 16:39:49 +1100

    update client to use the newer init api

--- example/simple/client.c
+++ example/simple/client.c
@@ -2,6 +2,10 @@
 #include <libmicrokitco.h>
 
 uintptr_t co_mem;
+int stack_size = 0x1000;
+uintptr_t stack1;
+uintptr_t stack2;
+uintptr_t stack3;
 
 void co_entry1() {
     microkit_dbg_puts("CO1: hi\n");
@@ -15,26 +19,18 @@ void co_entry3() {
     microkit_dbg_puts("CO3: hi\n");
     microkit_cothread_destroy_me();
 }
-void co_entry4() {
-    microkit_dbg_puts("CO4: hi\n");
-    microkit_cothread_destroy_me();
-}
-void co_entry5() {
-    microkit_dbg_puts("CO5: hi\n");
-    microkit_cothread_destroy_me();
-}
 
-void co_entry6() {
+void co_entry4() {
     // should not enter
-    microkit_dbg_puts("CO6: hi\n");
     *(char *) 0 = 0;
     microkit_cothread_destroy_me();
 }
 
 void init(void) {
     microkit_dbg_puts("CLIENT: starting\n");
-    
-    if (microkit_cothread_init((void *) co_mem, 0x40000, 6) != MICROKITCO_NOERR) {
+
+    co_err_t err = microkit_cothread_init(co_mem, stack_size, 3, stack1, stack2, stack3);
+    if (err != MICROKITCO_NOERR) {
         microkit_dbg_puts("ERR: cannot init libmicrokitco\n");
         return;
     }
@@ -43,17 +39,15 @@ void init(void) {
     int co1 = microkit_cothread_spawn(co_entry1, 0, 1);
     int co2 = microkit_cothread_spawn(co_entry2, 0, 1);
     int co3 = microkit_cothread_spawn(co_entry3, 0, 1);
-    int co4 = microkit_cothread_spawn(co_entry4, 0, 1);
-    int co5 = microkit_cothread_spawn(co_entry5, 0, 1);
 
-    if (microkit_cothread_spawn(co_entry6, 0, 1) != MICROKITCO_ERR_MAX_COTHREADS_REACHED) {
+    if (microkit_cothread_spawn(co_entry4, 0, 1) != MICROKITCO_ERR_MAX_COTHREADS_REACHED) {
         microkit_dbg_puts("ERR: was able to spawn more cothreads than allowed\n");
         return;
     }
 
     microkit_dbg_puts("CLIENT: cothreads spawned\n");
 
-    // should print the `COn: hi` 5x.
+    // should print the `COn: hi` 3x.
     microkit_cothread_yield();
 
     // This MUST print before the cothreads' prints because client is higher prio
