20005e693133f3246eee6cc2cbcff58c67c5a371
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-21 14:41:45 +1100

    fix silly err in client of codis example where notify is not in init

--- example/codis/client.c
+++ example/codis/client.c
@@ -9,8 +9,9 @@ void init(void) {
     uintptr_t client_num = (ipc >> 12) & 0xF;
 
     printf("CLIENT #%ld: starting\n", client_num);
+
+    microkit_notify(SERVER_CHANNEL);
 }
 
 void notified(microkit_channel channel) {
-    microkit_notify(SERVER_CHANNEL);
 }
