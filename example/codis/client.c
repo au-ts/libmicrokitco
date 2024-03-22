440fdaa838f65fd1f33274c110f0a5e92f4ce725
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-22 12:07:04 +1100

    reworked skeleton of client

--- example/codis/client.c
+++ example/codis/client.c
@@ -5,12 +5,38 @@
 
 uintptr_t ipc;
 
+char words[] = {
+    
+}
+
+// each client does a different thing in this example:
+void client1_tasks(char *ipc) {
+    printf("CLIENT #1: starting\n");
+}
+
+void client2_tasks(char *ipc) {
+    printf("CLIENT #2: starting\n");
+}
+
+void client3_tasks(char *ipc) {
+    printf("CLIENT #3: starting\n");
+}
+
+void client4_tasks(char *ipc) {
+    printf("CLIENT #4: starting\n");
+}
+
+void (*task[4])(char *) = {
+    client1_tasks,
+    client2_tasks,
+    client3_tasks,
+    client4_tasks
+};
+
 void init(void) {
+    char *our_ipc = (char *) ipc;
     uintptr_t client_num = (ipc >> 12) & 0xF;
-
-    printf("CLIENT #%ld: starting\n", client_num);
-
-    microkit_notify(SERVER_CHANNEL);
+    task[client_num - 1](our_ipc);
 }
 
 void notified(microkit_channel channel) {
