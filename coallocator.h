6e565724b18093d5a3e8ee0bdabf8de3b6e4b0a1
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-25 11:10:34 +1100

    use size_t for word size in allocator instead of hard coding to long long

--- coallocator.h
+++ coallocator.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <stddef.h>
 #include "util.h"
 
 // A very crude memory allocator for the coroutine library.
@@ -7,19 +8,19 @@
 typedef struct allocator {
     // memory is allocated bottom UP
     void *backing_memory;
-    word_t size;
+    size_t size;
 
     // next available address
-    word_t brk;
+    size_t brk;
 
-    void *(*alloc)(struct allocator *, word_t);
+    void *(*alloc)(struct allocator *, size_t);
 } allocator_t;
 
-void *co_alloc(allocator_t *allocator, word_t size) {
-    if (size < 1 || allocator->brk + size > allocator->size + (word_t) allocator->backing_memory) {
+void *co_alloc(allocator_t *allocator, size_t size) {
+    if (size < 1 || allocator->brk + size > allocator->size + (size_t) allocator->backing_memory) {
         return 0;
     } else {
-        word_t ret = allocator->brk;
+        size_t ret = allocator->brk;
         allocator->brk += size;
         return (void *) ret;
     }
@@ -29,17 +30,15 @@ int co_allocator_init(void *backing_memory, int size, allocator_t *allocator) {
     if (!backing_memory || size < 1) {
         return 1;
     }
-
-    // memzero(backing_memory, size);
     
     // quick sanity check
     unsigned char *mem = (unsigned char *) backing_memory;
     mem[0] = 0;
-    mem[size - 1] = size;
+    mem[size - 1] = 0;
 
     allocator->backing_memory = backing_memory;
     allocator->size = size;
-    allocator->brk = (word_t) backing_memory;
+    allocator->brk = (size_t) backing_memory;
     allocator->alloc = co_alloc;
     return 0;
 }
