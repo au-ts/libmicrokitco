a502afe0f5285eba6a6b24595a8cd79a5a0426e7
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-25 11:15:42 +1100

    update simple example system for arg passing

--- example/simple/client.c
+++ example/simple/client.c
@@ -1,5 +1,6 @@
 #include <microkit.h>
 #include <libmicrokitco.h>
+#include <printf.h>
 
 uintptr_t co_mem;
 int stack_size = 0x1000;
@@ -7,51 +8,39 @@ uintptr_t stack1;
 uintptr_t stack2;
 uintptr_t stack3;
 
-void co_entry1() {
-    microkit_dbg_puts("CO1: hi\n");
-    microkit_cothread_destroy_me();
-}
-void co_entry2() {
-    microkit_dbg_puts("CO2: hi\n");
-    microkit_cothread_destroy_me();
-}
-void co_entry3() {
-    microkit_dbg_puts("CO3: hi\n");
-    microkit_cothread_destroy_me();
-}
-
-void co_entry4() {
-    // should not enter
-    *(char *) 0 = 0;
+void co_entry() {
+    size_t our_id;
+    microkit_cothread_get_arg(0, &our_id);
+    printf("CO%ld: hi\n", our_id);
 }
 
 void init(void) {
-    microkit_dbg_puts("CLIENT: starting\n");
+    printf("CLIENT: starting\n");
 
     co_err_t err = microkit_cothread_init(co_mem, stack_size, 3, stack1, stack2, stack3);
     if (err != MICROKITCO_NOERR) {
-        microkit_dbg_puts("ERR: cannot init libmicrokitco\n");
+        printf("ERR: cannot init libmicrokitco\n");
         return;
     }
-    microkit_dbg_puts("CLIENT: libmicrokitco started\n");
+    printf("CLIENT: libmicrokitco started\n");
 
     microkit_cothread_t co1, co2, co3, co4;
     
-    microkit_cothread_spawn(co_entry1, priority_false, ready_true, &co1);
-    microkit_cothread_spawn(co_entry2, priority_false, ready_true, &co2);
-    microkit_cothread_spawn(co_entry3, priority_false, ready_true, &co3);
+    microkit_cothread_spawn(co_entry, priority_false, ready_true, &co1, 1, 1);
+    microkit_cothread_spawn(co_entry, priority_false, ready_true, &co2, 1, 2);
+    microkit_cothread_spawn(co_entry, priority_false, ready_true, &co3, 1, 3);
 
-    if (microkit_cothread_spawn(co_entry4, priority_false, ready_true, &co4) != MICROKITCO_ERR_MAX_COTHREADS_REACHED) {
-        microkit_dbg_puts("ERR: was able to spawn more cothreads than allowed\n");
+    if (microkit_cothread_spawn(co_entry, priority_false, ready_true, &co4, 1, 4) != MICROKITCO_ERR_MAX_COTHREADS_REACHED) {
+        printf("ERR: was able to spawn more cothreads than allowed\n");
         return;
     }
 
-    microkit_dbg_puts("CLIENT: cothreads spawned\n");
+    printf("CLIENT: cothreads spawned\n");
 
     microkit_cothread_yield();
 
     // This prints before the cothreads' prints because root thread is higher priority.
-    microkit_dbg_puts("CLIENT: done, exiting!\n");
+    printf("CLIENT: done, exiting!\n");
 
     microkit_cothread_deprioritise(0);
     microkit_cothread_yield();
