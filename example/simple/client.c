d5176f48b73ac07fe12a1e3c780a86fff4b94553
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-03-25 16:23:25 +1100

    fix wrong err type in simple example

--- example/simple/client.c
+++ example/simple/client.c
@@ -18,7 +18,7 @@ void init(void) {
     printf("CLIENT: starting\n");
 
     co_err_t err = microkit_cothread_init(co_mem, stack_size, 3, stack1, stack2, stack3);
-    if (err != MICROKITCO_NOERR) {
+    if (err != co_no_err) {
         printf("ERR: cannot init libmicrokitco\n");
         return;
     }
@@ -30,7 +30,7 @@ void init(void) {
     microkit_cothread_spawn(co_entry, priority_false, ready_true, &co2, 1, 2);
     microkit_cothread_spawn(co_entry, priority_false, ready_true, &co3, 1, 3);
 
-    if (microkit_cothread_spawn(co_entry, priority_false, ready_true, &co4, 1, 4) != MICROKITCO_ERR_MAX_COTHREADS_REACHED) {
+    if (microkit_cothread_spawn(co_entry, priority_false, ready_true, &co4, 1, 4) == co_no_err) {
         printf("ERR: was able to spawn more cothreads than allowed\n");
         return;
     }
@@ -42,7 +42,6 @@ void init(void) {
     // This prints before the cothreads' prints because root thread is higher priority.
     printf("CLIENT: done, exiting!\n");
 
-    microkit_cothread_deprioritise(0);
     microkit_cothread_yield();
 
     // returns to Microkit event loop for recv'ing notifications.
