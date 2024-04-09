868af4e32b2e74d44785c444a9a802e7086589cb
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-04-09 13:08:45 +1000

    add microkit round trip notify benchmark system

---
+++ example/validation_notify/client.c
@@ -0,0 +1,54 @@
+#include <microkit.h>
+#include <sel4/benchmark_utilisation_types.h>
+#include <serial_drv/printf.h>
+#include "util.h"
+
+uintptr_t uart_base;
+
+uint64_t *ipcbuffer;
+
+#define PASSES 10
+uint64_t result[PASSES];
+int nth;
+
+void init(void) {
+    ipcbuffer = (uint64_t*) &(seL4_GetIPCBuffer()->msg[0]);
+    nth = 0;
+}
+
+void notified(microkit_channel channel) {
+    if (channel == 3) {
+        sddf_printf_("Starting round trip notify benchmark\n");
+
+        seL4_BenchmarkResetThreadUtilisation(TCB_CAP);
+        seL4_BenchmarkResetLog();
+
+        microkit_notify(1);
+        
+    } else if (channel == 1) {
+        seL4_BenchmarkFinalizeLog();
+        seL4_BenchmarkGetThreadUtilisation(TCB_CAP);
+        result[nth] = ipcbuffer[BENCHMARK_TOTAL_UTILISATION];
+        nth += 1;
+
+        if (nth == PASSES) {
+            sddf_printf_("Result:\n");
+            uint64_t accumulator = 0;
+            for (int i = 0; i < PASSES; i++) {
+                sddf_printf_("Pass #%d: %lu cycles\n", i, result[i]);
+                accumulator += result[i];
+            }
+            sddf_printf_("Average: %ld cycles\n", accumulator / PASSES);
+            sddf_printf_("FINISHED\n");
+        } else {
+
+            seL4_BenchmarkResetThreadUtilisation(TCB_CAP);
+            seL4_BenchmarkResetLog();
+
+            microkit_notify(1);
+        }
+
+    } else {
+        sddf_printf_("Received notification from unknown channel %d\n", channel);
+    }
+}
