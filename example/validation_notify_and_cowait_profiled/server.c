27a70de3419234e9008e4639f9ee31440b0acd09
Bill Nguyen <z5422337@ad.unsw.edu.au>
2024-04-11 16:36:46 +1000

    fix compile errors of the profiling systme

--- example/validation_notify_and_cowait_profiled/server.c
+++ example/validation_notify_and_cowait_profiled/server.c
@@ -1,11 +1,11 @@
 #include <microkit.h>
-#include <serial_drv/printf.h>
-#include "util.h"
+// #include <serial_drv/printf.h>
+// #include "util.h"
 
 uintptr_t uart_base;
 
 void init(void) {
-    sddf_printf_("SERVER: started\n");
+    // sddf_printf_("SERVER: started\n");
 }
 
 void notified(microkit_channel channel) {
