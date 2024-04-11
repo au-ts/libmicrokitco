8c2dba0969154bee2c9585b69a8f645981e467a9
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-04-11 15:30:45 +1000

    prep for profiling

--- example/validation_notify_and_cowait_profiled/client.c
+++ example/validation_notify_and_cowait_profiled/client.c
@@ -8,22 +8,22 @@ uintptr_t uart_base;
 uintptr_t co_mem;
 uintptr_t co_stack;
 
-uint64_t *ipcbuffer;
+// uint64_t *ipcbuffer;
 
 #define PASSES 10
-uint64_t result[PASSES];
+// uint64_t result[PASSES];
 
 void run(int nth) {
-    seL4_BenchmarkResetThreadUtilisation(TCB_CAP);
-    seL4_BenchmarkResetLog();
+    // seL4_BenchmarkResetThreadUtilisation(TCB_CAP);
+    // seL4_BenchmarkResetLog();
 
     microkit_notify(1);
     microkit_cothread_wait(1);
 
-    seL4_BenchmarkFinalizeLog();
-    seL4_BenchmarkGetThreadUtilisation(TCB_CAP);
+    // seL4_BenchmarkFinalizeLog();
+    // seL4_BenchmarkGetThreadUtilisation(TCB_CAP);
 
-    result[nth] = ipcbuffer[BENCHMARK_TOTAL_UTILISATION];
+    // result[nth] = ipcbuffer[BENCHMARK_TOTAL_UTILISATION];
 }
 
 size_t runner(void) {
@@ -31,17 +31,17 @@ size_t runner(void) {
     for (int i = 0; i < PASSES; i++) {
         run(i);
     }
-    sddf_printf_("Result:\n");
-    for (int i = 0; i < PASSES; i++) {
-        sddf_printf_("===> %lu\n", result[i]);
-    }
+    // sddf_printf_("Result:\n");
+    // for (int i = 0; i < PASSES; i++) {
+    //     sddf_printf_("===> %lu\n", result[i]);
+    // }
     sddf_printf_("FINISHED\n");
 
     return 0;
 }
 
 void init(void) {
-    ipcbuffer = (uint64_t*) &(seL4_GetIPCBuffer()->msg[0]);
+    // ipcbuffer = (uint64_t*) &(seL4_GetIPCBuffer()->msg[0]);
     co_err_t err = microkit_cothread_init(co_mem, 0x1000, 1, co_stack);
     if (err != co_no_err) {
         sddf_printf_("CLIENT: Cannot init libmicrokitco, err is :%s\n", microkit_cothread_pretty_error(err));
