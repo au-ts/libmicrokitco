5dc7d33c1b4050df14f9c4e1d5893a820db00a26
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-04-19 11:43:21 +1000

    fix sign error in error pretty printing facility

--- libmicrokitco.c
+++ libmicrokitco.c
@@ -63,11 +63,10 @@ const char *err_strs[] = {
 
 // Return a string of human friendly error message.
 const char *microkit_cothread_pretty_error(co_err_t err_num) {
-    int abs_err = err_num * -1;
-    if (err_num < 0 || abs_err >= co_num_errors) {
+    if (err_num < 0 || err_num >= co_num_errors) {
         return "libmicrokitco: unknown error!\n";
     } else {
-        return err_strs[abs_err];
+        return err_strs[err_num];
     }
 }
 
