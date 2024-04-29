c1b19024000fb6f5209c1fd2c4e4643dca976ad9
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-04-29 12:13:11 +1000

    validation_notify_and_bare_libco_wait: update readme, make server passive, made benchmarking rig more cache friendly

--- example/validation_notify_and_bare_libco_wait/client.c
+++ example/validation_notify_and_bare_libco_wait/client.c
@@ -9,44 +9,68 @@ uintptr_t co_stack;
 cothread_t root_handle;
 cothread_t co_handle;
 
-#define PASSES 20
-uint64_t result[PASSES];
+#define WARMUP_PASSES 8
+#define MEASURE_PASSES 32
 
-inline static void run(int nth) {
-    sel4bench_reset_counters();
+uint64_t sum_t;
+uint64_t sum_sq;
+uint64_t result;
+uint64_t prev_cycle_count;
 
+static void FASTFN run() {
     microkit_notify(1);
     co_switch(root_handle);
+}
 
-    result[nth] = sel4bench_get_cycle_count();
+static void FASTFN measure(int ith) {
+    prev_cycle_count = sel4bench_get_cycle_count();
+
+    run();
+
+    result = sel4bench_get_cycle_count() - prev_cycle_count;
+
+    sum_t += result;
+    sum_sq += result * result;
 }
 
 void runner(void) {
     sddf_printf_("Starting round trip notify - wait with bare libco - notify benchmark\n");
-    for (int i = 0; i < PASSES; i++) {
-        run(i);
+    for (int i = 0; i < WARMUP_PASSES; i++) {
+        run();
+    }
+    for (int i = 0; i < MEASURE_PASSES; i++) {
+        measure(i);
     }
 
     sddf_printf_("Result:\n");
-    for (int i = 0; i < PASSES; i++) {
-        sddf_printf_("===> %lu\n", result[i]);
-    }
+
+    sddf_printf_("Mean: %lf\n", sum_t / (float) MEASURE_PASSES);
+    sddf_printf_("Stdev = sqrt(%lf)\n", ((MEASURE_PASSES * sum_sq - (sum_t * sum_t)) / (float) (MEASURE_PASSES * (MEASURE_PASSES - 1))));
+
     sddf_printf_("FINISHED\n");
 
     co_switch(root_handle);
 }
 
 void init(void) {
-    sel4bench_init();
-
     root_handle = co_active();
     co_handle = co_derive((void *) co_stack, 0x1000, runner);
-
-    co_switch(co_handle);
 }
 
 void notified(microkit_channel channel) {
     if (channel == 1) {
         co_switch(co_handle);
+    } else if (channel == 3) {
+        // Start
+        
+        sel4bench_init();
+        sel4bench_get_cycle_count();
+        sum_t = 0;
+        sum_sq = 0;
+        result = 0;
+        prev_cycle_count = 0;
+
+        co_switch(co_handle);
     }
+
 }
