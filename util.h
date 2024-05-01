6c57695fd82f19426d89f0e0aee7a99ea703e6e0
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-05-01 11:06:30 +1000

    clean up co_memcpy

--- util.h
+++ util.h
@@ -6,8 +6,7 @@ void panic() {
 }
 
 void co_memcpy(void *dest, void *source, int size) {
-    unsigned char *destination = (unsigned char *) dest;
     for (int i = 0; i < size; i++) {
-        destination[i] = ((unsigned char *) source)[i];
+        ((unsigned char *) dest)[i] = ((unsigned char *) source)[i];
     }
 }
