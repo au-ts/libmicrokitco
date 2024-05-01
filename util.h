e9390137fdcb7bcaec6d2a0adf40459b6775d862
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-05-01 16:18:11 +1000

    remove co_memcpy after queue optimisation

--- util.h
+++ util.h
@@ -4,9 +4,3 @@ void panic() {
     char *panic_addr = (char *) NULL;
     *panic_addr = (char) 0;
 }
-
-void co_memcpy(void *dest, void *source, int size) {
-    for (int i = 0; i < size; i++) {
-        ((unsigned char *) dest)[i] = ((unsigned char *) source)[i];
-    }
-}
