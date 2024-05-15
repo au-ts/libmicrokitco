cd5ea25daea9dda115805119ff036ba43899f1fc
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-05-15 12:13:21 +1000

    Make stack canary 64 bits and fix a null deference

--- libco/riscv64.c
+++ libco/riscv64.c
@@ -9,13 +9,19 @@ extern "C"
 {
 #endif
 
-inline void panic() {
-    char *panic_addr = (char *) 0;
-    *panic_addr = (char) 0;
+inline void panic()
+{
+    char *panic_addr = (char *)0;
+    *panic_addr = (char)0;
 }
 
+// Cothread context memory layout:
+// Registers... | c_entry | pc | canary | ... | <- stack
+// If stack grows into canary then we crash!
+
 // control region indexes at the beginning of cothread local storage
-enum {
+enum
+{
     ra,
     sp,
     fp, // AKA s0
@@ -47,98 +53,97 @@ enum {
     canary
 };
 
-#define STACK_CANARY 0xBBADF00D
+#define STACK_CANARY 0x341294AA8642FE71
 
-// Quick reference: https://inst.eecs.berkeley.edu/~cs61c/fa17/img/riscvcard.pdf
-
-static thread_local uintptr_t co_active_buffer[64];
-static thread_local cothread_t co_active_handle = 0;
+static thread_local uintptr_t co_active_buffer[32];
+static thread_local cothread_t co_active_handle = &co_active_buffer;
 
 // co_swap(char *to, char *from)
 static void (*co_swap)(cothread_t, cothread_t) = 0;
 
+// Quick reference: https://inst.eecs.berkeley.edu/~cs61c/fa17/img/riscvcard.pdf
 section(text)
-const uint32_t co_swap_function[1024] = {
-    // Instructions encoded with this tool: https://luplab.gitlab.io/rvcodecjs/
+    const uint32_t co_swap_function[1024] = {
+        // Instructions encoded with this tool: https://luplab.gitlab.io/rvcodecjs/
 
-    // Begin saving callee saved registers of current context
+        // Begin saving callee saved registers of current context
 
-    // RV64I InsSet
-    0x0015b023, // sd ra, 0(a1)
-    0x0025b423, // sd sp, 8(a1)
-    0x0085b823, // sd s0, 16(a1)
-    0x0095bc23, // sd s1, 24(a1)
-    0x0325b023, // sd s2, 32(a1)
-    0x0335b423, // sd s3, 40(a1)
-    0x0345b823, // sd s4, 48(a1)
-    0x0355bc23, // sd s5, 56(a1)
-    0x0565b023, // sd s6, 64(a1)
-    0x0575b423, // sd s7, 72(a1)
-    0x0585b823, // sd s8, 80(a1)
-    0x0595bc23, // sd s9, 88(a1)
-    0x07a5b023, // sd s10, 96(a1)
-    0x07b5b423, // sd s11, 104(a1)
+        // RV64I InsSet
+        0x0015b023, // sd ra, 0(a1)
+        0x0025b423, // sd sp, 8(a1)
+        0x0085b823, // sd s0, 16(a1)
+        0x0095bc23, // sd s1, 24(a1)
+        0x0325b023, // sd s2, 32(a1)
+        0x0335b423, // sd s3, 40(a1)
+        0x0345b823, // sd s4, 48(a1)
+        0x0355bc23, // sd s5, 56(a1)
+        0x0565b023, // sd s6, 64(a1)
+        0x0575b423, // sd s7, 72(a1)
+        0x0585b823, // sd s8, 80(a1)
+        0x0595bc23, // sd s9, 88(a1)
+        0x07a5b023, // sd s10, 96(a1)
+        0x07b5b423, // sd s11, 104(a1)
 
-    // Save floating point saved register
-    // Unused because the Microkit SDK itself is soft-float
-    // We can't link soft float and hard float objects together
+        // Save floating point saved register
+        // Unused because the Microkit SDK itself is soft-float
+        // We can't link soft float and hard float objects together
 
-    // 0x0685b827, // fsd f8, 112(a1)  EQUIV fsd fs0, 112(a1)
-    // 0x0695bc27, // fsd f9, 120(a1)
-    // 0x0925b027, // fsd f18, 128(a1) 
-    // 0x0935b427, // fsd f19, 136(a1)
-    // 0x0945b827, // fsd f20, 144(a1)
-    // 0x0955bc27, // fsd f21, 152(a1)
-    // 0x0b65b027, // fsd f22, 160(a1)
-    // 0x0b75b427, // fsd f23, 168(a1)
-    // 0x0b85b827, // fsd f24, 176(a1)
-    // 0x0b95bc27, // fsd f25, 184(a1)
-    // 0x0da5b027, // fsd f26, 192(a1)
-    // 0x0db5b427, // fsd f27, 200(a1)
+        // 0x0685b827, // fsd f8, 112(a1)  EQUIV fsd fs0, 112(a1)
+        // 0x0695bc27, // fsd f9, 120(a1)
+        // 0x0925b027, // fsd f18, 128(a1)
+        // 0x0935b427, // fsd f19, 136(a1)
+        // 0x0945b827, // fsd f20, 144(a1)
+        // 0x0955bc27, // fsd f21, 152(a1)
+        // 0x0b65b027, // fsd f22, 160(a1)
+        // 0x0b75b427, // fsd f23, 168(a1)
+        // 0x0b85b827, // fsd f24, 176(a1)
+        // 0x0b95bc27, // fsd f25, 184(a1)
+        // 0x0da5b027, // fsd f26, 192(a1)
+        // 0x0db5b427, // fsd f27, 200(a1)
 
-    // When co_swap is called, `ra` have the PC we need to resume the `from` cothread!
-    0x0c15bc23, // sd ra, 216(a1)
+        // When co_swap is called, `ra` have the PC we need to resume the `from` cothread!
+        0x0c15bc23, // sd ra, 216(a1)
 
-    // Begin loading callee saved registers of the context we are switching to
-    0x00053083, // ld ra, 0(a0)
-    0x00853103, // ld sp, 8(a0)
-    0x01053403, // ld s0, 16(a0)
-    0x01853483, // ld s1, 24(a0)
-    0x02053903, // ld s2, 32(a0)
-    0x02853983, // ld s3, 40(a0)
-    0x03053a03, // ld s4, 48(a0)
-    0x03853a83, // ld s5, 56(a0)
-    0x04053b03, // ld s6, 64(a0)
-    0x04853b83, // ld s7, 72(a0)
-    0x05053c03, // ld s8, 80(a0)
-    0x05853c83, // ld s9, 88(a0)
-    0x06053d03, // ld s10, 96(a0)
-    0x06853d83, // ld s11, 104(a0)
+        // Begin loading callee saved registers of the context we are switching to
+        0x00053083, // ld ra, 0(a0)
+        0x00853103, // ld sp, 8(a0)
+        0x01053403, // ld s0, 16(a0)
+        0x01853483, // ld s1, 24(a0)
+        0x02053903, // ld s2, 32(a0)
+        0x02853983, // ld s3, 40(a0)
+        0x03053a03, // ld s4, 48(a0)
+        0x03853a83, // ld s5, 56(a0)
+        0x04053b03, // ld s6, 64(a0)
+        0x04853b83, // ld s7, 72(a0)
+        0x05053c03, // ld s8, 80(a0)
+        0x05853c83, // ld s9, 88(a0)
+        0x06053d03, // ld s10, 96(a0)
+        0x06853d83, // ld s11, 104(a0)
 
-    // 0x07053407, // fld f8, 112(a0)
-    // 0x07853487, // fld f9, 120(a0)
-    // 0x08053907, // fld f18, 128(a0)
-    // 0x08853987, // fld f19, 136(a0)
-    // 0x09053a07, // fld f20, 144(a0)
-    // 0x09853a87, // fld f21, 152(a0)
-    // 0x0a053b07, // fld f22, 160(a0)
-    // 0x0a853b87, // fld f23, 168(a0)
-    // 0x0b053c07, // fld f24, 176(a0)
-    // 0x0b853c87, // fld f25, 184(a0)
-    // 0x0c053d07, // fld f26, 192(a0)
-    // 0x0c853d87, // fld f27, 200(a0)
+        // 0x07053407, // fld f8, 112(a0)
+        // 0x07853487, // fld f9, 120(a0)
+        // 0x08053907, // fld f18, 128(a0)
+        // 0x08853987, // fld f19, 136(a0)
+        // 0x09053a07, // fld f20, 144(a0)
+        // 0x09853a87, // fld f21, 152(a0)
+        // 0x0a053b07, // fld f22, 160(a0)
+        // 0x0a853b87, // fld f23, 168(a0)
+        // 0x0b053c07, // fld f24, 176(a0)
+        // 0x0b853c87, // fld f25, 184(a0)
+        // 0x0c053d07, // fld f26, 192(a0)
+        // 0x0c853d87, // fld f27, 200(a0)
 
-    // load the PC of the destination context then jump to it
-    // discard link result
-    0x0d853603, // ld a2, 216(a0)
-    0x00060067, // jalr a2, 0(a2) 
-    // Note to reader, `jalr a2, a2` is not equivalent!
+        // load the PC of the destination context then jump to it
+        // discard link result
+        0x0d853603, // ld a2, 216(a0)
+        0x00060067, // jalr a2, 0(a2)
+        // Note to reader, `jalr a2, a2` is not equivalent!
 };
 
 static void co_entrypoint(void)
 {
     uintptr_t *buffer = (uintptr_t *)co_active_handle;
-    void (*entrypoint)(void) = (void (*)(void))buffer[26];
+    void (*entrypoint)(void) = (void (*)(void))buffer[client_entry];
     entrypoint();
     panic(); /* Panic if cothread_t entrypoint returns */
 }
@@ -157,9 +162,7 @@ cothread_t co_derive(void *memory, unsigned int size, void (*entrypoint)(void))
         co_active_handle = &co_active_buffer;
 
     if (!co_swap)
-    {
         co_swap = (void (*)(cothread_t, cothread_t))co_swap_function;
-    }
 
     handle = (uintptr_t *)memory;
     // 16-bit align "down" the stack ptr
@@ -167,39 +170,11 @@ cothread_t co_derive(void *memory, unsigned int size, void (*entrypoint)(void))
     uintptr_t *p = (uintptr_t *)((unsigned char *)handle + offset);
 
     handle[ra] = 0; /* crash if cothread return! */
-    handle[sp] = (uintptr_t)p; 
+    handle[sp] = (uintptr_t)p;
+    handle[fp] = (uintptr_t)p;
 
-    handle[fp] = (uintptr_t)p; 
-
-    // These aren't really needed, but keeping in for clarity
-    handle[s1] = 0;            
-    handle[s2] = 0;         
-    handle[s3] = 0;          
-    handle[s4] = 0;      
-    handle[s5] = 0;        
-    handle[s6] = 0;       
-    handle[s7] = 0;         
-    handle[s8] = 0;      
-    handle[s9] = 0;          
-    handle[s10] = 0;        
-    handle[s11] = 0;       
-
-    handle[f8] = 0;
-    handle[f9] = 0; 
-    handle[f18] = 0; 
-    handle[f19] = 0; 
-    handle[f20] = 0;
-    handle[f21] = 0; 
-    handle[f22] = 0; 
-    handle[f23] = 0;
-    handle[f24] = 0; 
-    handle[f25] = 0;
-    handle[f26] = 0;
-    handle[f27] = 0;
-
-    handle[client_entry] = (uintptr_t)entrypoint;    
-    handle[pc] = (uintptr_t)co_entrypoint; 
-
+    handle[client_entry] = (uintptr_t)entrypoint;
+    handle[pc] = (uintptr_t)co_entrypoint;
     handle[canary] = STACK_CANARY;
 
     return handle;
@@ -207,9 +182,9 @@ cothread_t co_derive(void *memory, unsigned int size, void (*entrypoint)(void))
 
 void co_switch(cothread_t handle)
 {
-    uintptr_t *memory = (uintptr_t *) handle;
-    if (memory[canary] != STACK_CANARY) {
-        // Crash if the cothread stack overflowed into our control region.
+    uintptr_t *memory = (uintptr_t *)handle;
+    if (co_active_buffer[canary] != STACK_CANARY || memory[canary] != STACK_CANARY)
+    {
         panic();
     }
 
