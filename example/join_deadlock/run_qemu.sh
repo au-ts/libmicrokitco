ee67cf69de194b112e29682c1edd2bc20d70747d
Bill <dreamliner787-9@bills-mbp.keg.cse.unsw.edu.au>
2024-05-30 15:01:14 +1000

    update run_qemu.sh of join_deadlock system to use toolchains on builder machine

--- example/join_deadlock/run_qemu.sh
+++ example/join_deadlock/run_qemu.sh
@@ -1,16 +1,19 @@
 # Change these for your setup!
-MICROKIT_SDK='/Users/dreamliner787-9/TS/sdk'
-MICROKIT_X86_64_SDK='/Users/dreamliner787-9/TS/sdk_x86_64'
-TOOLCHAIN_PATH_PREFIX=''
+MICROKIT_SDK='/home/billn/microkit-sdk-1.2.6-patched'
+MICROKIT_X86_64_SDK='/home/billn/microkit-sdk-1.2.6-x86-64'
+A64N_TOOLCHAIN_PATH_PREFIX='/opt/toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/'
+A64L_TOOLCHAIN_PATH_PREFIX=''
+R64_TOOLCHAIN_PATH_PREFIX='/opt/toolchain/riscv/bin/'
+X64_TOOLCHAIN_PATH_PREFIX=''
 
 echo "Choose your system to run:"
 echo "1. QEMU aarch64 - Build with LLVM"
 echo "2. QEMU riscv64 - Build with LLVM"
-echo "3. QEMU aarch64 - Build with aarch64-none-elf triple"
-echo "4. QEMU aarch64 - Build with aarch64-unknown-linux-gnu triple"
-echo "5. QEMU riscv64 - Build with riscv64-unknown-elf triple"
+echo "3. QEMU aarch64 - Build with aarch64-none-elf-gcc"
+echo "4. QEMU aarch64 - Build with aarch64-linux-gnu-gcc"
+echo "5. QEMU riscv64 - Build with riscv64-unknown-elf-gcc"
 
-echo "Enter your option: \c"
+echo -n "Enter your option: "
 read option
 if [ $option = 1 ] 
 then
@@ -22,14 +25,14 @@ then
 
 elif [ $option = 3 ]
 then
-    make run_qemu_aarch64 TARGET=aarch64-none-elf TOOLCHAIN="${TOOLCHAIN_PATH_PREFIX}aarch64-none-elf" MICROKIT_SDK="$MICROKIT_SDK"
+    make run_qemu_aarch64 TARGET=aarch64-none-elf TOOLCHAIN="${A64N_TOOLCHAIN_PATH_PREFIX}aarch64-none-elf" MICROKIT_SDK="$MICROKIT_SDK"
 
 elif [ $option = 4 ]
 then
-    make run_qemu_aarch64 TARGET=aarch64-unknown-linux-gnu TOOLCHAIN="${TOOLCHAIN_PATH_PREFIX}aarch64-unknown-linux-gnu" MICROKIT_SDK="$MICROKIT_SDK"
+    make run_qemu_aarch64 TARGET=aarch64-linux-gnu TOOLCHAIN="${A64L_TOOLCHAIN_PATH_PREFIX}aarch64-linux-gnu" MICROKIT_SDK="$MICROKIT_SDK"
 
 elif [ $option = 5 ]
 then
-    make run_qemu_riscv64 TARGET=riscv64-unknown-elf TOOLCHAIN="${TOOLCHAIN_PATH_PREFIX}riscv64-unknown-elf" MICROKIT_SDK="$MICROKIT_SDK"
+    make run_qemu_riscv64 TARGET=riscv64-unknown-elf TOOLCHAIN="${R64_TOOLCHAIN_PATH_PREFIX}riscv64-unknown-elf" MICROKIT_SDK="$MICROKIT_SDK"
 
 fi
