ea40f098e67ea7f102f3afb5c642915c9cf6535e
Bill Nguyen <billn@builder.keg.cse.unsw.edu.au>
2024-06-03 16:42:26 +1000

    single source of truth for SDK

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 export A64_TOOLCHAIN='/opt/toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-none-elf/bin/aarch64-none-elf'
-export R64_TOOLCHAIN=/opt/toolchain/riscv/bin/riscv64-unknown-elf
+export R64_TOOLCHAIN='/opt/toolchain/riscv/bin/riscv64-unknown-elf'
+export SDK='/home/billn/microkit-sdk-1.2.6-patched'
 export OPENSBI='/home/billn/opensbi'
 
 for benchmark in example/validation_*
