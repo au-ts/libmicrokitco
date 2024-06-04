6572b8ce0c83da6eae8a1ec48c8f489327aa9042
Bill Nguyen <billn@builder.keg.cse.unsw.edu.au>
2024-06-04 10:43:26 +1000

    throw stderr into report file as well

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -5,30 +5,40 @@ export R64_TOOLCHAIN='/opt/toolchain/riscv/bin/riscv64-unknown-elf'
 export SDK='/home/billn/microkit-sdk-1.2.6-patched'
 export OPENSBI='/home/billn/opensbi'
 
-for benchmark in example/validation_*
-do
+run_odroidc4 () {
     (
         # Run Odroid C4
         cd $benchmark && \
-        ./run_mq.sh odroidc4 >report.txt && \
+        ./run_mq.sh odroidc4 &>report.txt && \
         echo "Odroid C4 - $benchmark:" && \
         grep -E "(Mean|Stdev)" <report.txt && exit 0
+    )
+}
+
+run_hifive () {
+    (
+        # Run HiFive
+        cd $benchmark && \
+        ./run_mq.sh hifive &>report.txt && \
+        echo "HiFive Unleashed - $benchmark:" && \
+        grep -E "(Mean|Stdev)" <report.txt && exit 0
     ) 
+}
 
-    if [ $? = 0 ];
+for benchmark in example/validation_*
+do
+    if [ "$1" = "odroidc4" ];
     then
-
-        (
-            # Run HiFive
-            cd $benchmark && \
-            ./run_mq.sh hifive >report.txt && \
-            echo "HiFive Unleashed - $benchmark:" && \
-            grep -E "(Mean|Stdev)" <report.txt && exit 0
-        ) 
-
+        run_odroidc4
+    elif [ "$1" = "hifive" ];
+    then
+        run_hifive
+    elif [ "$1" = "both" ];
+    then
+        run_odroidc4
+        run_hifive
     else
-
+        echo "unknown option"
         break
-
     fi
 done
