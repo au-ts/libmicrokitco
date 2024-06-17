9214ac61ad3f2aa231c54e89212f4c90c954edb1
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-06-17 10:05:14 +1000

    specify host OS of bundled SDK for benchmarking

--- example/benchmarks/run_benchmarks.sh
+++ example/benchmarks/run_benchmarks.sh
@@ -6,7 +6,7 @@ export A64_TOOLCHAIN='/opt/toolchain/arm-gnu-toolchain-12.2.rel1-x86_64-aarch64-
 export R64_TOOLCHAIN='/home/billn/riscv64-unknown-elf-toolchain-10.2.0-2020.12.8-x86_64-linux-ubuntu14/bin/riscv64-unknown-elf'
 
 # Don't change these
-export SDK=$(realpath microkit-sdk-1.2.6-libmicrokitco-bench-riscv_patched)
+export SDK=$(realpath microkit-sdk-1.2.6-linux_x86_64-libmicrokitco-bench-riscv_patched)
 export OPENSBI=$(realpath opensbi)
 
 run_odroidc4 () {
