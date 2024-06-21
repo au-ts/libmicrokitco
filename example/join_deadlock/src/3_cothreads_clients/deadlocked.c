86e7eea88139603e12a4d6cf687596a50c1ad4cb
Bill Nguyen <bill.nguyen@student.unsw.edu.au>
2024-06-21 16:51:23 +1000

    fix join deadlock example system to use the new APIs
    Signed-off-by: Bill Nguyen <bill.nguyen@student.unsw.edu.au>

--- example/join_deadlock/src/3_cothreads_clients/deadlocked.c
+++ example/join_deadlock/src/3_cothreads_clients/deadlocked.c
@@ -8,41 +8,40 @@ uintptr_t stack1;
 uintptr_t stack2;
 uintptr_t stack3;
 
+size_t cothreads_return_codes[4] = { 0 };
+
 #define MAGIC 0x42
 
-size_t co_entry1() {
-    size_t retval;
+void co_entry1() {
     printf("Deadlocked PD: co1: joining co2\n");
-    co_err_t err = microkit_cothread_join(2, &retval);
+    co_err_t err = microkit_cothread_join(2);
     if (err != co_no_err) {
         printf("Deadlocked PD: co1: cannot join, err: %s\n", microkit_cothread_pretty_error(err));
     }
-    return retval;
+    cothreads_return_codes[1] = cothreads_return_codes[2];
 };
 
-size_t co_entry2() {
-    size_t retval;
+void co_entry2() {
     printf("Deadlocked PD: co2: joining co3\n");
-    co_err_t err = microkit_cothread_join(3, &retval);
+    co_err_t err = microkit_cothread_join(3);
     if (err != co_no_err) {
         printf("Deadlocked PD: co1: cannot join, err: %s\n", microkit_cothread_pretty_error(err));
     }
-    return retval;
+    cothreads_return_codes[2] = cothreads_return_codes[3];
 };
 
-size_t co_entry3() {
-    size_t _retval;
+void co_entry3() {
     printf("Deadlocked PD: co3: joining co1\n");
     // 1 -> 2 -> 3 \, deadlocked!
     // ^-----------|
-    co_err_t err = microkit_cothread_join(1, &_retval);
+    co_err_t err = microkit_cothread_join(1);
     if (err == co_err_join_deadlock_detected) {
         printf("Deadlocked PD: co3: DEADLOCK DETECTED...OK\n");
     } else {
         printf("Deadlocked PD: co3: ERROR: DEADLOCK NOT DETECTED\n");
         microkit_internal_crash(err);
     }
-    return MAGIC;
+    cothreads_return_codes[3] = MAGIC;
 };
 
 void init(void) {
@@ -69,15 +68,14 @@ void init(void) {
 
     printf("Deadlocked PD: cothreads spawned\n");
 
-    size_t retval;
     printf("Deadlocked PD: root: joining co1\n");
-    err = microkit_cothread_join(1, &retval);
+    err = microkit_cothread_join(1);
     if (err != co_no_err) {
         printf("Deadlocked PD: root: cannot join, err is %s\n", microkit_cothread_pretty_error(err));
-    } else if (retval == MAGIC){
+    } else if (cothreads_return_codes[1] == MAGIC){
         printf("Deadlocked PD: root: finished ok\n");
     } else {
-        printf("Deadlocked PD: root: magic does not match %p\n", retval);
+        printf("Deadlocked PD: root: magic does not match %p\n", cothreads_return_codes[1]);
     }
 }
 
--- example/join_deadlock/src/4_cothreads_clients/many.c
+++ example/join_deadlock/src/4_cothreads_clients/many.c
@@ -9,54 +9,53 @@ uintptr_t stack2;
 uintptr_t stack3;
 uintptr_t stack4;
 
+size_t cothreads_return_codes[5] = { 0 };
+
 #define MAGIC 0x42
 
 // entrypoints #1, #2 and #3 will join #4
-size_t co_entry1() {
-    size_t retval;
+void co_entry1() {
     printf("Many to one PD: co1: joining co4\n");
-    co_err_t err = microkit_cothread_join(4, &retval);
+    co_err_t err = microkit_cothread_join(4);
     if (err != co_no_err) {
         printf("Many to one PD: co1: cannot join, err: %s\n", microkit_cothread_pretty_error(err));
-    } else if (retval != MAGIC) {
-        printf("Many to one PD: co1: wrong magic returned %p\n", retval);
+    } else if (cothreads_return_codes[4] != MAGIC) {
+        printf("Many to one PD: co1: wrong magic returned %p\n", cothreads_return_codes[4]);
     } else {
         printf("Many to one PD: co1: awaken with correct magic\n");
     }
-    return retval >> 4;
+    cothreads_return_codes[1] = cothreads_return_codes[4] >> 4;
 };
 
-size_t co_entry2() {
-    size_t retval;
+void co_entry2() {
     printf("Many to one PD: co2: joining co4\n");
-    co_err_t err = microkit_cothread_join(4, &retval);
+    co_err_t err = microkit_cothread_join(4);
     if (err != co_no_err) {
         printf("Many to one PD: co2: cannot join, err: %s\n", microkit_cothread_pretty_error(err));
-    } else if (retval != MAGIC) {
-        printf("Many to one PD: co2: wrong magic returned %p\n", retval);
+    } else if (cothreads_return_codes[4] != MAGIC) {
+        printf("Many to one PD: co2: wrong magic returned %p\n", cothreads_return_codes[4]);
     } else {
         printf("Many to one PD: co2: awaken with correct magic\n");
     }
-    return retval >> 8;
+    cothreads_return_codes[2] = cothreads_return_codes[4] >> 8;
 };
 
-size_t co_entry3() {
-    size_t retval;
+void co_entry3() {
     printf("Many to one PD: co3: joining co4\n");
-    co_err_t err = microkit_cothread_join(4, &retval);
+    co_err_t err = microkit_cothread_join(4);
     if (err != co_no_err) {
         printf("Many to one PD: co3: cannot join, err: %s\n", microkit_cothread_pretty_error(err));
-    } else if (retval != MAGIC) {
-        printf("Many to one PD: co3: wrong magic returned %p\n", retval);
+    } else if (cothreads_return_codes[4] != MAGIC) {
+        printf("Many to one PD: co3: wrong magic returned %p\n", cothreads_return_codes[4]);
     } else {
         printf("Many to one PD: co3: awaken with correct magic\n");
     }
-    return retval >> 12;
+    cothreads_return_codes[3] = cothreads_return_codes[4] >> 12;
 };
 
-size_t co_entry4() {
+void co_entry4() {
     printf("Many to one PD: co4 returning magic\n");
-    return MAGIC;
+    cothreads_return_codes[4] = MAGIC;
 };
 
 void init(void) {
@@ -85,13 +84,17 @@ void init(void) {
 
     printf("Many to one PD: cothreads %d %d %d %d spawned\n", co1, co2, co3, co4);
 
-    size_t retval1, retval2, retval3;
     printf("Many to one PD: root: joining co1\n");
-    co_err_t join_err1 = microkit_cothread_join(1, &retval1);
+    co_err_t join_err1 = microkit_cothread_join(1);
     printf("Many to one PD: root: joining co2\n");
-    co_err_t join_err2 = microkit_cothread_join(2, &retval2);
+    co_err_t join_err2 = microkit_cothread_join(2);
     printf("Many to one PD: root: joining co3\n");
-    co_err_t join_err3 = microkit_cothread_join(3, &retval3);
+    co_err_t join_err3 = microkit_cothread_join(3);
+
+    size_t retval1 = cothreads_return_codes[1];
+    size_t retval2 = cothreads_return_codes[2];
+    size_t retval3 = cothreads_return_codes[3];
+
     if (join_err1 != co_no_err || join_err2 != co_no_err || join_err3 != co_no_err) {
         printf("Many to one PD: root: cannot do one or more joins, errs are %d %d %d\n", join_err1, join_err2, join_err3);
         microkit_internal_crash(0);
@@ -103,7 +106,7 @@ void init(void) {
     }
 
     // joining again after reading the retval should yield err
-    if (microkit_cothread_join(3, &retval3) != co_err_generic_not_initialised) {
+    if (microkit_cothread_join(3) != co_err_generic_not_initialised) {
         printf("Many to one PD: root: was able to re-read retval\n");
         microkit_internal_crash(0);
     }
