e629aa8af44f865f4bc2791c15eb7df8c1f701e1
dumsum <dumsum@pm.me>
2025-10-08 13:05:00 +1100

    Build multiple variants with different configurations
    Much like in lib_sddf_lwip, by defining appropriate
    `LIBMICROKITCO_CFLAGS_*` variables including different
    `libmicrokitco_opts.h`, multiple variants can be built.
    
    Also adds libc include prerequisite.
    
    Signed-off-by: dumsum <dumsum@pm.me>

--- libmicrokitco.mk
+++ libmicrokitco.mk
@@ -1,5 +1,43 @@
+#
 # Copyright 2025, UNSW
+#
 # SPDX-License-Identifier: BSD-2-Clause
+#
+# This Makefile snippet builds the libmicrokitco library.
+# The library provides a synchronous API over the asynchronous
+# event handlers provided by the seL4 Microkit.
+#
+# NOTES:
+# Generates libmicrokitco.a or libmicrokitco%.a. That is,
+# it can generate different versions of the library with
+# the same filename stem. This is useful if you want to
+# build multiple different versions with different build-
+# time configurations. CFLAGS can be configured using the
+# LIBMICROKITCO_CFLAGS variable, as well as its suffixed
+# variant.
+#
+# For example, if you only need to generate one version
+# of the library, usage is simple:
+#
+# LIBMICROKITCO_CFLAGS := -O2 -I/path/to/libmicrokitco_opts.h
+# include libmicrokitco.mk
+# my_program.elf: libmicrokitco.a
+#
+# However, if you have two different programs requiring
+# two differently configured versions of the library, you
+# can do this instead:
+#
+# LIBMICROKITCO_CFLAGS_0 := -O0 -g -I/path/to/program_0/libmicrokitco_opts.h
+# LIBMICROKITCO_CFLAGS_1 := -O3 -I/path/to/program_1/libmicrokitco_opts.h
+# include libmicrokitco.mk
+# program_0.elf: libmicrokitco_0.a
+# program_1.elf: libmicrokitco_1.a
+#
+# Observe that each LIBMICROKITCO_CFLAGS variant should contain the
+# include path for the respective libmicrokitco_opts.h.
+#
+# LIBMICROKITCO_LIBC_INCLUDE can be set to a build target for the C standard
+# library include directory if providing your own libc implementation.
 
 ifndef MICROKIT_SDK
 $(error MICROKIT_SDK is not set)
@@ -22,26 +60,48 @@ ifndef LIBMICROKITCO_PATH
 $(error LIBMICROKITCO_PATH is not set)
 endif
 
-# Absolute path to the directory containing libmicrokitco_opts.h
-ifndef LIBMICROKITCO_OPT_PATH
-$(error LIBMICROKITCO_OPT_PATH is not set)
-endif
-
 ifndef LIBCO_PATH
 LIBCO_PATH := $(LIBMICROKITCO_PATH)/libco
 endif
 
-$(BUILD_DIR)/libmicrokitco/libco.o: $(LIBCO_PATH)/libco.c $(LIBCO_PATH)/libco.h $(LIBCO_PATH)/aarch64.c $(LIBCO_PATH)/amd64.c $(LIBCO_PATH)/arm.c $(LIBCO_PATH)/riscv64.c $(LIBCO_PATH)/settings.h |$(BUILD_DIR)/libmicrokitco
-	$(CC) $(CFLAGS) -c -Wno-unused-value $< -o $@
+VARIANTS ?= $(patsubst LIBMICROKITCO_CFLAGS%,%,$(filter LIBMICROKITCO_CFLAGS%,$(.VARIABLES)))
 
-$(BUILD_DIR)/libmicrokitco/libmicrokitco.o: $(LIBMICROKITCO_PATH)/libmicrokitco.c $(LIBMICROKITCO_PATH)/libmicrokitco.h $(LIBMICROKITCO_PATH)/libhostedqueue/libhostedqueue.h $(LIBMICROKITCO_OPT_PATH)/libmicrokitco_opts.h
-	$(CC) $(CFLAGS) -c -I$(LIBCO_PATH) -I$(LIBMICROKITCO_OPT_PATH) $< -o $@
+LIBCO_DEPS := \
+  $(LIBCO_PATH)/libco.c \
+  $(LIBCO_PATH)/libco.h \
+  $(LIBCO_PATH)/aarch64.c \
+  $(LIBCO_PATH)/amd64.c \
+  $(LIBCO_PATH)/arm.c \
+  $(LIBCO_PATH)/riscv64.c \
+  $(LIBCO_PATH)/settings.h
 
-$(BUILD_DIR)/libmicrokitco.a: $(BUILD_DIR)/libmicrokitco/libco.o $(BUILD_DIR)/libmicrokitco/libmicrokitco.o
-	$(LD) -r $^ -o $@
+LIBMICROKITCO_DEPS := \
+  $(LIBMICROKITCO_PATH)/libmicrokitco.c \
+  $(LIBMICROKITCO_PATH)/libmicrokitco.h \
+  $(LIBMICROKITCO_PATH)/libhostedqueue/libhostedqueue.h
 
-$(BUILD_DIR)/libmicrokitco:
-	mkdir -p $(BUILD_DIR)/libmicrokitco
+define MICROKITCO_RECIPE
 
-clobber::
-	rm -f $(BUILD_DIR)/libmicrokitco.a
+LIBDIR$(1) := $(BUILD_DIR)/libmicrokitco$(1)
+OBJS$(1) := $$(LIBDIR$(1))/libco.o $$(LIBDIR$(1))/libmicrokitco.o
+LIB$(1) := libmicrokitco$(1).a
+
+$$(LIBDIR$(1)):
+	mkdir -p $$@
+
+$$(LIBDIR$(1))/libco.o: $$(LIBCO_DEPS) | $$(LIBDIR$(1))
+	$$(CC) $$(CFLAGS) $$(LIBMICROKITCO_CFLAGS$(1)) -c -Wno-unused-value $(LIBCO_PATH)/libco.c -o $$@
+
+$$(LIBDIR$(1))/libmicrokitco.o: $$(LIBMICROKITCO_DEPS) | $$(LIBDIR$(1)) $$(LIBMICROKITCO_LIBC_INCLUDE)
+	$$(CC) $$(CFLAGS) $$(LIBMICROKITCO_CFLAGS$(1)) -c -I$$(LIBCO_PATH) $(LIBMICROKITCO_PATH)/libmicrokitco.c -o $$@
+
+$$(LIB$(1)): $$(OBJS$(1))
+	$$(LD) -r $$^ -o $$@
+
+endef
+
+ifneq ($(VARIANTS),)
+$(foreach v,$(VARIANTS),$(eval $(call MICROKITCO_RECIPE,$(v))))
+else
+$(eval $(call MICROKITCO_RECIPE,))
+endif
